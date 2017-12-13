d18ea57951fb896c0d6442d8891125aa88a97b2d
banestyrelsen <valdemarjohansen@gmail.com>
2017-12-13 12:20:44 +0100

    #2

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -3,19 +3,42 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
-	public static void main(String[] args) {
-		DisplayManager.createDisplay();
+  public static void main(String[] args) {
+    DisplayManager.createDisplay();
 
-		while (!Display.isCloseRequested()) {
-			// game logic
-			// render
-			DisplayManager.updateDisplay();
-		}
+    Loader loader = new Loader();
+    Renderer renderer = new Renderer();
 
-		DisplayManager.closeDisplay();
-	}
+    // OpenGL expects vertices to be defined counter clockwise by default
+    float[] vertices = {
+        // Left bottom triangle
+        -0.5f, 0.5f, 0f, //
+        -0.5f, -0.5f, 0f, //
+        0.5f, -0.5f, 0f, //
+        // Right top triangle
+        0.5f, -0.5f, 0f, //
+        0.5f, 0.5f, 0f, //
+        -0.5f, 0.5f, 0f //
+    };
+    
+    RawModel model = loader.loadToVAO(vertices);
+
+    while (!Display.isCloseRequested()) {
+      renderer.prepare();
+      // game logic
+      // render
+      renderer.render(model);
+      DisplayManager.updateDisplay();
+    }
+
+    loader.cleanUp();
+    DisplayManager.closeDisplay();
+  }
 
 }
