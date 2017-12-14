49dc3f6bdac0942708c0ee375186b9a7a4fa94bf
valdemarjohansen@gmail.com <valdemarjohansen@gmail.com>
2017-12-14 09:12:53 +0100

    #10

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -21,114 +22,15 @@ public class MainGameLoop {
     StaticShader shader = new StaticShader();
     Renderer renderer = new Renderer(shader);
 
-    // OpenGL expects vertices to be defined counterclockwise by default
-    float[] vertices = {      
-                        -0.5f,0.5f,-0.5f,  //
-                        -0.5f,-0.5f,-0.5f,  
-                        0.5f,-0.5f,-0.5f, 
-                        0.5f,0.5f,-0.5f,    
-                        
-                        -0.5f,0.5f,0.5f,  //
-                        -0.5f,-0.5f,0.5f, //
-                        0.5f,-0.5f,0.5f,  
-                        0.5f,0.5f,0.5f,
-                        
-                        0.5f,0.5f,-0.5f,  
-                        0.5f,-0.5f,-0.5f, 
-                        0.5f,-0.5f,0.5f,  
-                        0.5f,0.5f,0.5f,
-                        
-                        -0.5f,0.5f,-0.5f, 
-                        -0.5f,-0.5f,-0.5f,  
-                        -0.5f,-0.5f,0.5f, 
-                        -0.5f,0.5f,0.5f,
-                        
-                        -0.5f,0.5f,0.5f,
-                        -0.5f,0.5f,-0.5f,
-                        0.5f,0.5f,-0.5f,
-                        0.5f,0.5f,0.5f,
-                        
-                        -0.5f,-0.5f,0.5f,
-                        -0.5f,-0.5f,-0.5f,
-                        0.5f,-0.5f,-0.5f,
-                        0.5f,-0.5f,0.5f
-                        
-                    };
-                    
-                    float[] textureCoords = {
-                        
-                        0,0,
-                        0,1,
-                        1,1,
-                        1,0,      
-                        0,0,
-                        0,1,
-                        1,1,
-                        1,0,      
-                        0,0,
-                        0,1,
-                        1,1,
-                        1,0,
-                        0,0,
-                        0,1,
-                        1,1,
-                        1,0,
-                        0,0,
-                        0,1,
-                        1,1,
-                        1,0,
-                        0,0,
-                        0,1,
-                        1,1,
-                        1,0
-
-                        
-                    };
-                    
-                    int[] indices = {
-                        0,1,3,  
-                        3,1,2,  
-                        4,5,7,
-                        7,5,6,
-                        8,9,11,
-                        11,9,10,
-                        12,13,15,
-                        15,13,14, 
-                        16,17,19,
-                        19,17,18,
-                        20,21,23,
-                        23,21,22
-
-                    };
-    
-    
-//    float[] vertices = { //
-//                         -0.5f, 0.5f, 0f, // V0
-//                         -0.5f, -0.5f, 0f, // V1
-//                         0.5f, -0.5f, 0f, // V2
-//                         0.5f, 0.5f, 0f // V3
-//    };
-//
-//    int[] indices = { 0, 1, 3, // Top left triangle (V0, V1, V3)
-//                      3, 1, 2 // Bottom right triangle (V3, V1, V2)
-//    };
-//
-//    // Attachment points for texture
-//    float[] textureCoords = { //
-//                              0, 0, // V0
-//                              0, 1, // V1
-//                              1, 1, // V2
-//                              1, 0 // V3
-//    };
-
-    RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-    ModelTexture texture = new ModelTexture(loader.loadTexture("spots"));
+    RawModel model = OBJLoader.loadObjModel("stall", loader);
+//    RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+    ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
     TexturedModel staticModel = new TexturedModel(model, texture);
-    Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+    Entity entity = new Entity(staticModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
     Camera camera = new Camera();
 
     while (!Display.isCloseRequested()) {
-      entity.increaseRotation(1, 1, 0);
+      entity.increaseRotation(0, 1, 0);
       camera.move();
       // entity.increaseRotation(0, 1, 0);
       renderer.prepare();
